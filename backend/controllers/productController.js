071ea5c813b4208f8b2257c8305d442714f6f2b7
mray1111 <manishcode111@gmail.com>
2023-09-02 23:02:16 +0530

    basic creation/step 1 is done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product=require("../models/productModels");
 
 
-//create Product
+//create Product -- Admin only admin can access
 exports.createProduct= async(req, res, next)=>{
     const product=await Product.create(req.body);
     res.status(201).json({
@@ -11,9 +11,85 @@ exports.createProduct= async(req, res, next)=>{
     console.log(product);
 }
 
-console.log(Product)
 
-exports.getAllProducts= (req, res) =>{
-    res.status(200).json({message:"Route is working fine"})
+
+//get all products
+exports.getAllProducts= async (req, res) =>{
+
+    const products= await Product.find();
+
+    res.status(200).json({
+        success:true,
+        products
+    })
 }
 
+//get single Product
+exports.getProductDetails= async (req, res, next)=>{
+    const product=await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product Not found"
+        })
+    }
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+
+}
+
+//Update Product -- Admin
+exports.updateProduct=async (req, res, next)=>{
+    let product=await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product Not Found"
+        })
+    }
+
+    product=await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    });
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+
+
+
+// Delete Product
+exports.deleteProduct = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        await product.deleteOne(); // Use the deleteOne method
+
+        res.status(200).json({
+            success: true,
+            message: "Product Deleted Successfully"
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "An error occurred while deleting the product"
+        });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,3 @@ connectDatabase();
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
-
