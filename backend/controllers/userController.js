5302e574b2275dc16bae9ee06cb13c12a0cf429b
mray1111 <manishcode111@gmail.com>
2023-09-05 22:49:13 +0530

    LOGIN is added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const User=require("../models/userModel");
-
+const catchAsyncErrors=require("../middleware/catchAsyncErrors")
 
 //Register a User
 exports.registerUser=async(req,res,next)=>{
@@ -24,3 +24,37 @@ exports.registerUser=async(req,res,next)=>{
         user
     });
 };
+
+//login User
+exports.loginUser= catchAsyncErrors(async(req, res, next)=>{
+    const {email, password}=req.body;
+
+        //checking if user has given password and email both
+        if(!email || !password){
+            return next(new ErrorHander("Please enter Email & Password",400));
+        }
+
+        const user=await User.findOne({email}).select("+password");
+        //console.log(user);
+
+        if(!user){
+            return next(new ErrorHander("Invalid email or Password "));
+        }
+
+        const isPasswordMatched= await user.comparePassword(password);
+        //console.log(isPasswordMatched);
+
+        if(!isPasswordMatched){
+            return next(new ErrorHander("Invalid email or password ",401));
+        }
+
+       // console.log(isPasswordMatched);
+
+    const token=user.getJWTToken();
+    //console.log(token)
+    res.status(201).json({
+        success:true,
+        token,
+        user
+    });
+})
---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,5 @@
+const sendToken=(user,statuscode,res)=>{
+    const token=user.getJWTToken();
+
+    //options for cookie
+}
