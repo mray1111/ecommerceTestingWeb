d35c279a2250b41d522735e6e6cd63cbaac5b18e
mray1111 <manishcode111@gmail.com>
2023-10-19 11:44:31 +0530

    ProductDetais.js Done 6.17

--- frontend/src/component/product/productDetails.js
+++ frontend/src/component/product/productDetails.js
@@ -1,23 +1,51 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Carousel from "react-material-ui-carousel";
-import "./ProductDetails.css";
+import ReactStars from 'react-rating-stars-component';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { getProductDetails } from "../../actions/productActions";
+import "./ProductDetails.css";
 
 const ProductDetails = () => {
-  const { id } = useParams(); // Use useParams to get the id parameter from the URL
+  const { id } = useParams();
   const dispatch = useDispatch();
   const { product, loading, error } = useSelector((state) => state.productDetails);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
-    dispatch(getProductDetails(id)); // Use the id from useParams
+    dispatch(getProductDetails(id));
   }, [dispatch, id]);
+
+  const options = {
+    edit: false,
+    color: "rgba(20,20,20,0.1)",
+    activeColor: "tomato",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product.ratings,
+    isHalf: true,
+  };
+
+  const increaseQuantity = () => {
+    if (product.Stock <= quantity) return;
+    const qty = quantity + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQuantity = () => {
+    if (1 >= quantity) return;
+    const qty = quantity - 1;
+    setQuantity(qty);
+  };
+
+  // Define the function for submitting reviews
+  const submitReviewToggle = () => {
+    // Implement your logic for submitting reviews
+  };
 
   return (
     <Fragment>
       <div className="ProductDetails">
-        <div>
+        <div className="ProductDetails-left">
           <Carousel>
             {product.images &&
               product.images.map((item, i) => (
@@ -29,6 +57,47 @@ const ProductDetails = () => {
                 />
               ))}
           </Carousel>
+        </div>
+
+        <div className="ProductDetails-right">
+          <div className="detailsBlock-1">
+            <h2>{product.name}</h2>
+            <p>Product # {product._id}</p>
+          </div>
+
+          <div className="detailsBlock-2">
+            <ReactStars {...options} />
+            <span className="detailsBlock-2-span">
+              {" "}
+              ({product.numOfReviews} Reviews)
+            </span>
+          </div>
+
+          <div className="detailsBlock-3">
+            <h1>₹{product.price}</h1>
+            <div className="detailsBlock-3-1">
+              <div className="detailsBlock-3-1-1">
+                <button onClick={decreaseQuantity}>-</button>
+                <input readOnly type="number" value={quantity} />
+                <button onClick={increaseQuantity}>+</button>
+              </div>
+              <button onClick={submitReviewToggle}>Add to Cart</button>
+              <p>
+                Status:
+                <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                  {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                </b>
+              </p>
+            </div>
+          </div>
+
+          <div className="detailsBlock-4">
+            Description : <p>{product.description}</p>
+          </div>
+
+          <button onClick={submitReviewToggle} className="submitReview">
+            Submit Review
+          </button>
         </div>
       </div>
     </Fragment>
