e6d5a2c20481f47f93fd4a4d976e18734a303f24
Purushottam Priyam Rathaur <111699939+rathauriitg@users.noreply.github.com>
2023-10-21 19:10:15 +0530

    All Product crating and filtering , pagenation, rating i. e all filtering done

--- frontend/src/component/product/Products.js
+++ frontend/src/component/product/Products.js
@@ -1,18 +1,34 @@
-import React, { Fragment, useEffect,useState } from 'react';
-import "./Products.css";
-import { useParams } from "react-router-dom"; // Use 'useParams' from 'react-router-dom'
-import { useSelector, useDispatch } from "react-redux";
-import { clearErrors, getProduct } from "../../actions/productActions";
-import Loader from "../layout/loader/Loader";
-import ProductCard from "../Home/ProductCard";
-import Pagination from "react-js-pagination";
+import React, { Fragment, useEffect, useState } from 'react';
+import './Products.css';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearErrors, getProduct } from '../../actions/productActions';
+import Loader from '../layout/loader/Loader';
+import ProductCard from '../Home/ProductCard';
+import Pagination from 'react-js-pagination';
 import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
 
+const categories = [
+  'Laptop',
+  'Footwear',
+  'Bottom Wear',
+  'Tops',
+  'Tshirts',
+  'Books',
+  'SmartPhone',
+  'Camera',
+  'Perfume',
+  'Kurta',
+];
 
-
-const Products = ({match}) => {
+const Products = ({ match }) => {
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState('');
+
+  const [ratings, setRatings] = useState(0);
 
   const {
     products,
@@ -20,19 +36,21 @@ const Products = ({match}) => {
     error,
     productsCount,
     resultPerPage,
-    filteredProductsCount
+    filteredProductsCount,
   } = useSelector((state) => state.products);
 
-  const { keyword } = useParams(); // Use 'useParams' to access route parameters
- 
+  const { keyword } = useParams();
 
-
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
 
   useEffect(() => {
-    dispatch(getProduct(keyword));
-  }, [dispatch, keyword]);
-
-  
+    dispatch(getProduct(keyword, currentPage, price, category));
+  }, [dispatch, keyword, currentPage, price, category]);
 
   return (
     <Fragment>
@@ -40,14 +58,75 @@ const Products = ({match}) => {
         <Loader />
       ) : (
         <Fragment>
-          <h2 className='productsHeading'>Products</h2>
-          <div className="products">
-          {products && products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-          
+          <h2 className="productsHeading">Products</h2>
+          <div className="container">
+            <div className="products">
+              {products &&
+                products.map((product) => (
+                  <ProductCard key={product._id} product={product} />
+                ))}
+            </div>
+
+            <div className="filterBox">
+              <Typography>Price</Typography>
+
+              <Slider
+                value={price}
+                onChange={priceHandler}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+                min={0}
+                max={25000}
+              />
+
+              <Typography>Categories</Typography>
+              <ul className="categoryBox">
+                {categories.map((category) => (
+                  <li
+                    className="category-link"
+                    key={category}
+                    onClick={() => setCategory(category)}
+                  >
+                    {category}
+                  </li>
+                ))}
+              </ul>
+              <fieldset>
+                <Typography component="legend" id="rating-legend">
+                  Ratings Above
+                </Typography>
+                <Slider
+                  value={ratings}
+                  onChange={(e, newRating) => {
+                    setRatings(newRating);
+                  }}
+                  aria-labelledby="rating-legend"
+                  valueLabelDisplay='auto'
+                  min={0}
+                  max={5}
+                />
+              </fieldset>
+            </div>
           </div>
 
+          {resultPerPage < productsCount && (
+            <div className="paginationBox">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productsCount}
+                onChange={setCurrentPageNo}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
+            </div>
+          )}
         </Fragment>
       )}
     </Fragment>
@@ -55,3 +134,9 @@ const Products = ({match}) => {
 };
 
 export default Products;
+
+
+ 
+
+ 
+
